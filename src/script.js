7585808d3ac845bd40730d558cc6746b4931ffff
AlexKritDev <akrit.developer@gmail.com>
2021-11-04 12:46:11 +0200

    upd

--- src/script.js
+++ src/script.js
@@ -1,15 +1,80 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 import * as dat from 'dat.gui'
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader";
+
+// Debug
+const gui = new dat.GUI();
+
+const debugObj = {
+    envMapIntensity: 5
+};
+
+//upd all materials
+
+const updateAllMaterials = () => {
+    scene.traverse((child) => {
+        if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+            // child.material.envMap = environmentMap
+            child.material.envMapIntensity = debugObj.envMapIntensity
+            child.material.needsUpdate = true;
+            child.receiveShadow = true;
+            child.castShadow = true;
+        }
+    })
+}
+
+//loaders
+const gltfLoader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('/draco/')
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+const environmentMap = cubeTextureLoader
+    .setPath('/textures/environmentMaps/1/')
+    .load([
+        'px.jpg',
+        'nx.jpg',
+        'py.jpg',
+        'ny.jpg',
+        'pz.jpg',
+        'nz.jpg'
+    ])
+environmentMap.encoding = THREE.sRGBEncoding;
+
+// dracoLoader.load(
+//     '/models/my_hamburger.glb',
+//     (file) => {
+//         console.log(file)
+//     },
+//     ()=>{
+//
+//     },
+//     (err)=>{
+//         console.log(err)
+//     }
+// )
+gltfLoader.setDRACOLoader(dracoLoader)
+gltfLoader.load(
+    // '/models/FlightHelmet/glTF/FlightHelmet.gltf',
+    '/models/my_hamburger.glb',
+    (gltf) => {
+        const helmet = gltf.scene;
+        helmet.scale.set(1, 1, 1)
+        helmet.position.set(0, -1, 0)
+        helmet.rotation.y = Math.PI * .5;
+        scene.add(helmet)
+
+        updateAllMaterials()
+        gui.add(helmet.rotation, 'y', -Math.PI, Math.PI, .001).name('rotation')
+    }
+)
+
 
 /**
  * Base
  */
-// Debug
-const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -17,56 +82,27 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Models
- */
-const dracoLoader = new DRACOLoader()
-dracoLoader.setDecoderPath('/draco/')
+scene.background = environmentMap;
+scene.environment = environmentMap;
 
-const gltfLoader = new GLTFLoader()
-gltfLoader.setDRACOLoader(dracoLoader)
-
-let mixer = null
-
-gltfLoader.load(
-    '/hamburger.glb',
-    (gltf) =>
-    {
-        scene.add(gltf.scene)
-    }
-)
-
-/**
- * Floor
- */
-const floor = new THREE.Mesh(
-    new THREE.PlaneBufferGeometry(50, 50),
-    new THREE.MeshStandardMaterial({
-        color: '#444444',
-        metalness: 0,
-        roughness: 0.5
-    })
-)
-floor.receiveShadow = true
-floor.rotation.x = - Math.PI * 0.5
-scene.add(floor)
-
-/**
- * Lights
- */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
-scene.add(ambientLight)
-
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
-directionalLight.castShadow = true
-directionalLight.shadow.mapSize.set(1024, 1024)
-directionalLight.shadow.camera.far = 15
-directionalLight.shadow.camera.left = - 7
-directionalLight.shadow.camera.top = 7
-directionalLight.shadow.camera.right = 7
-directionalLight.shadow.camera.bottom = - 7
-directionalLight.position.set(5, 5, 5)
+// light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3)
+directionalLight.position.set(0.25, 3, -2.25)
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.mapSize.set(512, 512)
+directionalLight.shadow.normalBias = .05;
 scene.add(directionalLight)
+
+// const directionalLightCameraHelper = new THREE.CameraHelper(directionalLight.shadow.camera)
+// scene.add(directionalLightCameraHelper)
+gui.add(directionalLight, 'intensity', 0, 10, .001).name('nameIntensity')
+gui.add(directionalLight.position, 'x', -5, 5, .01).name('lightX')
+gui.add(directionalLight.position, 'y', -5, 5, .01).name('lightY')
+gui.add(directionalLight.position, 'z', -5, 5, .01).name('lightZ')
+
+gui.add(debugObj, 'envMapIntensity', 0, 10, .001).onChange(updateAllMaterials)
+
 
 /**
  * Sizes
@@ -76,8 +112,7 @@ const sizes = {
     height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
+window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
     sizes.height = window.innerHeight
@@ -96,42 +131,45 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(- 8, 4, 8)
+camera.position.set(4, 1, -4)
 scene.add(camera)
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 1, 0)
 controls.enableDamping = true
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    antialias: true
 })
-renderer.shadowMap.enabled = true
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 3;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+gui.add(renderer, 'toneMapping', {
+    NO: THREE.NoToneMapping,
+    Linear: THREE.LinearToneMapping,
+    Reinhard: THREE.ReinhardToneMapping,
+    Cineon: THREE.CineonToneMapping,
+    ACESFilimc: THREE.ACESFilmicToneMapping,
+}).onChange(() => {
+    renderer.toneMapping = Number(renderer.toneMapping)
+    updateAllMaterials()
+})
+gui.add(renderer, 'toneMappingExposure', 0, 10, .01)
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-let previousTime = 0
-
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
-    const deltaTime = elapsedTime - previousTime
-    previousTime = elapsedTime
-
-    if(mixer)
-    {
-        mixer.update(deltaTime)
-    }
-
+const tick = () => {
     // Update controls
     controls.update()
 
